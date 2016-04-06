17cec0c87e4ed6141f3995b9c899591cd8092f79
bibi21000 <bibi21000@gmail.com>
2016-04-06 23:35:49 +0200

    Update bus

--- setup.py
+++ setup.py
@@ -105,7 +105,6 @@ setup(
     dependency_links = [
       'https://github.com/bibi21000/janitoo/archive/master.zip#egg=janitoo',
       'https://github.com/bibi21000/janitoo_raspberry/archive/master.zip#egg=janitoo_raspberry',
-      'https://github.com/bibi21000/janitoo_raspberry_i2c/archive/master.zip#egg=janitoo_raspberry_i2c',
       'https://github.com/adafruit/Adafruit_Python_GPIO/archive/master.zip#egg=Adafruit-GPIO',
       'https://github.com/adafruit/Adafruit_Python_ILI9341/archive/master.zip#egg=Adafruit_ILI9341',
     ],
