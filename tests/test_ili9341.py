88b58c9b870bce08fc60d1ccbdc03ad995935fd6
bibi21000 <bibi21000@gmail.com>
2016-04-21 21:48:41 +0200

    Update tests

--- tests/test_ili9341.py
+++ tests/test_ili9341.py
@@ -44,7 +44,6 @@ from janitoo.utils import TOPIC_NODES, TOPIC_NODES_REPLY, TOPIC_NODES_REQUEST
 from janitoo.utils import TOPIC_BROADCAST_REPLY, TOPIC_BROADCAST_REQUEST
 from janitoo.utils import TOPIC_VALUES_USER, TOPIC_VALUES_CONFIG, TOPIC_VALUES_SYSTEM, TOPIC_VALUES_BASIC
 
-import janitoo_raspberry_ili9341.bus_ili9341
 import janitoo_raspberry_ili9341.ili9341
 
 class TestLcdcharComponent(JNTTComponent, JNTTComponentCommon):
